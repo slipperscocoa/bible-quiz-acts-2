4f1fb5533e052e1288c12a3c0e71864fe7da47f0
Emily Harnish <72752507+slipperscocoa@users.noreply.github.com>
2024-08-06 14:53:06 -0400

    laptop: background image

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,18 @@ import './Home.css';
 
 class HomeApp extends React.Component {
     render() {
-        return(
+        return (
             <div className='home-custom'>
-                <br />
-                <h1>Welcome to TBQuizWhiz!</h1>
-                <br />
-                <h3>This is an app created by a former quizzer for future quizzers</h3>
-                <br/>
-                <h3>It offers a variety of games designed to help you learn and retain scripture in an enjoyable and meaningful way</h3>
-                <br/>
-                <h3>Use the Menu to select your first game</h3>
+                <div className='content'>
+                    <br />
+                    <h1>Welcome to TBQuizWhiz!</h1>
+                    <br />
+                    <h3>This is an app created by a former quizzer for future quizzers</h3>
+                    <br />
+                    <h3>It offers a variety of games designed to help you learn and retain scripture in an enjoyable and meaningful way</h3>
+                    <br />
+                    <h3>Use the Menu to select your first game</h3>
+                </div>
             </div>
         )
     }
